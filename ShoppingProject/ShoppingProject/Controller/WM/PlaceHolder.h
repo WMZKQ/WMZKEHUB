d63c13a7e308f9955bc949e3193619d66aa122fb
Nick007 <mac@hakunamatata.local>
2016-07-04 10:06:42 +0800

    wm
    新增水印，保证分支存活....

---
+++ ShoppingProject/ShoppingProject/Controller/WM/PlaceHolder.h
@@ -0,0 +1,13 @@
+//
+//  PlaceHolder.h
+//  ShoppingProject
+//
+//  Created by Mac on 16/7/4.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlaceHolder : NSObject
+
+@end
---
+++ ShoppingProject/ShoppingProject/View/ZKQ/placeholder.m
@@ -0,0 +1,13 @@
+//
+//  placeholder.m
+//  ShoppingProject
+//
+//  Created by Mac on 16/7/4.
+//  Copyright © 2016年 lyq. All rights reserved.
+//
+
+#import "placeholder.h"
+
+@implementation placeholder
+
+@end
